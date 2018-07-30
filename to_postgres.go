2e867b8858e422df7c2e75a75f5519271dc9eb19
Marcin Jachymiak <marcin@bitcoinops.org>
2018-07-30 10:19:29 -0400

    Tweak postgres insertion

--- to_postgres.go
+++ to_postgres.go
@@ -15,11 +15,26 @@ func toPostgres() {
 			   This function goes through all json files in db-dump,
 		decodes the file to a DashboardData struct, and then inserts it into a postgresql table
 	*/
+	DB := os.Getenv("DB")
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_ADDR, ok := os.LookupEnv("DB_ADDR")
+	if !ok {
+		switch DB_USED {
+		case "influxdb":
+			DB_ADDR = "http://localhost:8086"
+		case "postgresql":
+			DB_ADDR = "http://localhost:5432"
+		}
+	}
 
 	db := pg.Connect(&pg.Options{
-		User:     "postgres",
-		Password: "slamdunk",
+		Addr:     DB_ADDR,
+		User:     DB_USERNAME,
+		Password: DB_PASSWORD,
+		Database: DB, // TODO: set this up properly
 	})
+
 	defer db.Close()
 
 	model := interface{}((*DashboardData)(nil))
@@ -36,7 +51,7 @@ func toPostgres() {
 		log.Fatal(err)
 	}
 
-	dataDir := currentDir + "/db-dump"
+	dataDir := currentDir + "/db-backup"
 	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
 		return
 	}
